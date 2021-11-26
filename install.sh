55e4e628de59197ec4b1bb28c4f8ca97cd8f81d5
asikeero <eero.asikainen@mail.com>
2021-11-26 22:17:52 +0200

    update

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# exit if any operation fails
+set -e
+
+# exit if missing parameters
+set -u
+
+# colors and printing functions
+NORM='\033[0m'
+GRE='\033[0;32m'
+RED='\033[0;31m'
+YEL='\033[0;33m'
+
+function gecho {
+    echo -e "${GRE}[message] $1${NORM}"
+}
+
+function yecho {
+    echo -e "${YEL}[warning] $1${NORM}"
+}
+
+function recho {
+    echo -e "${RED}[error] $1${NORM}"
+    exit 1
+}
+
+# check prerequisites
+function check_prereq {
+    (command -v $1 > /dev/null && gecho "prereq $1 found") ||
+        recho "prereq $1 not found, install first"
+}
+
+# look for cmdline tool, if not found install
+function install {
+    (command -v $1 > /dev/null && gecho "$1 found, not installing") || 
+        (yecho "$1 not found, installing" && apt install $1 -y)
+}
+
+# actions start here
+# check directory
+[[ $(basename $(pwd)) == ".dotfiles" ]] ||
+    recho "directory should be ~/.dotfiles/"
+
+# update apt repos
+apt update
+apt upgrade
+
+# check prerequisites
+check_prereq git
+
+# install wanted tools
+install stow
+install neofetch
+
+
+
+gecho "Install finished"
