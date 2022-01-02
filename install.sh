237568a945076a3de6204314e229f84526136bcf
asikeero <60272147+asikeero@users.noreply.github.com>
2022-01-02 19:11:25 +0200

    kitty and nvim added

--- install.sh
+++ install.sh
@@ -53,13 +53,27 @@ apt update
 
 # check prerequisites
 check_prereq git
+check_prereq curl
 
 # install wanted tools
 install neofetch
 install stow
-
+install neovim
+install tree
+install ranger
 
 # create needed symlinks with stow
 stow --adopt */
+
+# install kitty terminal and add desktop integration
+homedir=/home/$SUDO_USER
+# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in your path
+ln -sf ${homedir}/.local/kitty.app/bin/kitty ${homedir}/.local/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ${homedir}/.local/kitty.app/share/applications/kitty.desktop ${homedir}/.local/share/applications/
+# Update the path to the kitty icon in the kitty.desktop file
+sed -i "s|Icon=kitty|Icon=/home/$SUDO_USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ${homedir}/.local/share/applications/kitty.desktop
+# set as default
+update-alternatives --set x-terminal-emulator ${homedir}/.local/bin/kitty
 
 gecho "Install finished!"
