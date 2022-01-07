b7e5b42f4251ae6d5baf52e8b76d9a390c9a6bfd
asikeero <60272147+asikeero@users.noreply.github.com>
2022-01-07 18:06:56 +0200

    update

--- install/nvim-configure.sh
+++ install/nvim-configure.sh
@@ -1,3 +1,4 @@
 # Configure nvim (expext nvim to be a valid command)
+~/.local/bin/nvim.appimage --headless -c q
 ~/.local/bin/nvim.appimage --headless -c "PackerSync" -c q
 ~/.local/bin/nvim.appimage --headless -c "LspInstall --sync pyright bashls" -c q
