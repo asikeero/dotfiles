484b1dd7261587403f458129d9f8a0ac033013ae
asikeero <60272147+asikeero@users.noreply.github.com>
2022-02-01 21:54:11 +0200

    update

--- nvim/.config/nvim/lua/eero/debugging.lua
+++ nvim/.config/nvim/lua/eero/debugging.lua
@@ -27,7 +27,7 @@ dap.configurations.python = {
       elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
         return cwd .. '/.venv/bin/python'
       else
-        return 'python'
+        return 'python3'
       end
     end;
   },
